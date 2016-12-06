54636bf9af17189404e3c25c8d5de7ec0c5cc79e
Yaoyuan Liu <lyy910619@gmail.com>
2016-12-06 08:50:01 -0500

    Save score path

--- games/empty/assets/js/saver.js
+++ games/empty/assets/js/saver.js
@@ -2,7 +2,7 @@ var $ = require('jQuery');
 module.exports = function () {
     return{
         saveScore: function (playerName, score) {
-            $.get( "http://cims.nyu.edu/~by653/hps/dbman/save.php",
+            $.get( "dbman/save.php",
                 { gamename: "DigThat", playername: playerName, score: score},
                 function() {
             }).done(function() {
